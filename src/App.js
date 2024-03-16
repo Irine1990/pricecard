81982f7f1e3197e5f5cfd34ac9b5e2bbb5ef4a49
Irine1990 <Irine1990@gmail.com>
2024-03-16 12:06:58 +0530

    PriceCard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div className="bg">
+        <div class="container text-center" >
+          <div class="row align-items-start">
+            <div class="col" id="tag1">
+              <h6>FREE</h6>
+              <h2>$0/month</h2><br></br>
+              <p class="text-start">✔️Single User</p>
+              <p class="text-start">✔️50GB Storage</p>
+              <p class="text-start">✔️Unlimited Public Projects</p>
+              <p class="text-start">✔️Community Access</p>
+              <p class="opacity-50 text-start">❌Unlimited Private Projects</p>
+              <p class="opacity-50 text-start">❌Dedicated Phone Support</p>
+              <p class="opacity-50 text-start">❌Free Subdomain</p>
+              <p class="opacity-50 text-start">❌Monthly Status Reports</p>
+              <div class="d-grid opacity-50">
+             <button class="btn btn-primary" type="button" >Button</button>
+              </div>
+            </div>
+
+
+            <div class="col" id="tag2">
+              <h6>PLUS</h6>
+              <h2>$/month</h2><br></br>
+              <p class="text-start" >✔️5 Users</p>
+              <p class="text-start">✔️50GB Storage</p>
+              <p class="text-start">✔️Unlimited Public Projects</p>
+              <p class="text-start">✔️Community Access</p>
+              <p class="text-start">✔️Unlimited Private Projects</p>
+              <p class="text-start">✔️Dedicated Phone Support</p>
+              <p class="text-start">✔️Free Subdomain</p>
+              <p class="opacity-50 text-start">❌Monthly Status Reports</p>
+              <div class="d-grid opacity-75">
+             <button class="btn btn-primary" type="button">Button</button>
+            </div>
+            </div>
+
+
+            <div class="col" id="tag3">
+              <h6>PRO</h6>
+              <h2>$49/month</h2><br></br>
+              <p class="text-start">✔️50GB Storage</p>
+              <p class="text-start">✔️Unlimited Users</p>
+              <p class="text-start">✔️Unlimited Public Projects</p>
+              <p class="text-start">✔️Community Access</p>
+              <p class="text-start">✔️Unlimited Private Projects</p>
+              <p class="text-start">✔️Dedicated Phone Support</p>
+              <p class="text-start">✔️Free Subdomain</p>
+              <p class="text-start">✔️Monthly Status Reports</p>
+              <div class="d-grid ">
+              <button class="btn btn-primary" type="button">Button</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
